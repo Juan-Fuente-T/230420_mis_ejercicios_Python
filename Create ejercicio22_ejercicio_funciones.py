0cd2b7ff488240034484659e0241c552e21cea9c
Juan_Fuente_T <jfuentet@gmail.com>
2023-04-21 23:55:26 +0200

    23.04.21

--- Create ejercicio22_ejercicio_funciones.py
+++ Create ejercicio22_ejercicio_funciones.py
@@ -1,4 +1,7 @@
-"""contenido_fichero = input ("Introduce la ruta del fichero: ")
+"""Ejercicio:
+Crear una función que reciba un nombre de fichero (la ruta completa)
+y devuelva un str con el contenido.
+contenido_fichero = input ("Introduce la ruta del fichero: ")
 
 def get_fichero(contenido:str):
     fichero = open(contenido_fichero, encoding="utf-8", mode="r")
@@ -8,46 +11,37 @@ def get_fichero(contenido:str):
     
 contenidos = get_fichero(contenido_fichero)
 print (contenidos)"""
-    
-    #Crear una función que reciba un nombre de fichero (la ruta completa)
-#y devuelva un str con el contenido.
 
-#Crear una función que reciba un str y un nombre de fichero (la ruta completa)
-#y .almacene el str en el fichero.
+'''Crear una función que reciba un str y cambie las letras a por @'''
 
-def escribir_fichero(texto:str, nombre_fichero):
-    contenido_fichero = input ("Introduce un texto: "," y un nombre de fichero" )
-    fichero = open(contenido_fichero, encoding="utf-8", mode="w")
-    info = fichero.write(texto)
+
+string = input("Ingrese un string: ")
+
+def transformar(string):
+   string.replace ("a", "@")
+ 
+
+arrobas =transformar(string)
+print (arrobas)
+
+
+
+
+
+'''Crear una función que reciba un str y un nombre de fichero (la ruta completa)
+y almacene el str en el fichero.'''
+
+
+"""def escribir_fichero(string:str, ruta_archivo):
+    fichero = open(ruta_archivo, encoding="utf-8", mode="w")
+    info = fichero.write(string)
     fichero.close()
     return info
-    
-contenidos = get_fichero(fichero)
-print (contenidos)
 
-def escribir_archivo(ruta, contenido):
-    archivo = open(ruta, "w")
-    archivo.write(contenido)
-    archivo.close()
+string = input ("Introduce un texto: " )
+ruta_archivo = input ("¿Donde quieres guardarlo?: ")
 
-FORGA
-contenido_fichero = input ("Introduce un texto: "," y un nombre de fichero" )
-
-def escribir_fichero(texto:str, nombre_fichero):
-    fichero = open(nombre_fichero, encoding="utf-8", mode="w")
-    info = fichero.write(texto)
-    fichero.close()
-    return info
-    
-contenido = escribir_fichero("iuhihh",C:\Users\Juan\Desktop\proba.txt)
-print (contenido)
-def sumar(sumando1, sumando2):
-    print(sumando1 + sumando2)
-
-sumar(3,4)
-
-
-
+escribir_fichero (string, ruta_archivo)"""
 
 
 
